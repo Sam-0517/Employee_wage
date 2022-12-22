d9069e3dc8872455d3458f888440d4f858dda416
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-22 17:16:22 +0530

    Added : Switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,21 +9,28 @@ public class EmployeeWage {
 		final int FULL_DAY = 8;
 		final int PART_TIME = 4;
 		int pay = 0;
-		double num = Math.floor((Math.random() * 10)) % 3;
+		int num = (int) (Math.floor((Math.random() * 10)) % 3);
 		System.out.println(num);
-		if (num == 1) {
+		
+		switch(num) {
+		case 1:
+		{
 			System.out.println("Employee is Present ");
 			pay = WAGE_PER_HR * FULL_DAY;
 			
 		}
-		else if(num == 2){
+		break;
+		case 2: {
 			  System.out.println("Employee is part-time");
 			  pay = WAGE_PER_HR * PART_TIME;
 		}
-		else{
+		break;
+		case 0:{
 			System.out.println("Employee is Absent ");
 		}
+		}
 		System.out.println("Employee Daily wage is "+pay);
+		
 	}
 
 }
