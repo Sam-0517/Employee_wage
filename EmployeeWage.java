8bad1267f3e99af3e7227c7203ccc86928c70c91
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-22 15:22:42 +0530

    Added : Welcome message

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz.Employee_wage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
