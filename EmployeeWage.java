4c780eec43788c3b3eccdeabcbeef014fdc3beab
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-22 15:41:40 +0530

    Added : Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,14 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
-
+		double num = Math.floor((Math.random()*10))%2;
+		//System.out.println(num);
+		
+		if(num == 1){
+			System.out.println("Employee is Present ");
+		}
+		else 
+			System.out.println("Employee is Absent ");
 	}
 
 }
