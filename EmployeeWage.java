3b43aa97c923585c3f5895b9d8a6fdc17216a790
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-28 08:35:12 +0530

    Added: Refactor code to class method to compute EmpWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,21 +1,19 @@
 package com.BridgeLabz.Employee_wage;
 
 public class EmployeeWage {
+	final int WAGE_PER_HR = 20;
+	final int WORKING_DAYS = 20;
+	int max_emp_hr = 100;
+	int emp_hr = 0;
+	int dailyWage = 0;
+	int totalEmpWage = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Program");
-		final int WAGE_PER_HR = 20;
-		final int WORKING_DAYS = 20;
-		int max_emp_hr = 100;
-		int emp_hr = 0;
-		int dailyWage = 0;
-		int totalEmpWage = 0;
+	int total_emp_hr = 0;
+	int total_working_days = 1;
+	int empCheck = 0;
 
-		int total_emp_hr = 0;
-		int total_working_days = 1;
-		int empCheck =0;
-		while(total_emp_hr  <= max_emp_hr && total_working_days  <= WORKING_DAYS){
+	public void emplpoyeeWage() {
+		while (total_emp_hr <= max_emp_hr && total_working_days <= WORKING_DAYS) {
 
 			empCheck = (int) (Math.floor((Math.random() * 10)) % 3);
 			switch (empCheck) {
@@ -31,13 +29,24 @@ public class EmployeeWage {
 				emp_hr = 0;
 
 			}
-		
-			total_emp_hr = total_emp_hr+emp_hr;
-			System.out.println("Total days "+total_working_days+ " Total working hrs "+total_emp_hr);
+
+			total_emp_hr = total_emp_hr + emp_hr;
+			System.out.println("Total days " + total_working_days
+					+ " Total working hrs " + total_emp_hr);
 			total_working_days++;
 		}
-			totalEmpWage = total_emp_hr*WAGE_PER_HR;
+		totalEmpWage = total_emp_hr * WAGE_PER_HR;
 		System.out.println("Monthly wage is " + totalEmpWage);
+
+	}
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		EmployeeWage obj = new EmployeeWage();
+		obj.emplpoyeeWage();
 	}
 
 }
